5639103da24f7d88e43d400d158c00edb97999b3
Santiago Acuña <snt-26@hotmail.com>
2022-04-13 17:41:30 -0500

    Add entities

---
+++ API.AzureFunctions/API.AzureFunctions.Functions/Entities/TodoEntity.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.AzureFunctions.Functions.Entities
+{
+    public class TodoEntity : TableEntity
+    {
+        public DateTime CreatedTime { get; set; }
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
