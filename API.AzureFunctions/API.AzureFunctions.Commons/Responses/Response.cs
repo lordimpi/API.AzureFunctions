03eb18f131199e31d5802b41119c90e9149e2f7c
Santiago Acuña <snt-26@hotmail.com>
2022-04-13 22:53:23 -0500

    Update Tasks

--- API.AzureFunctions/API.AzureFunctions.Commons/Responses/Response.cs
+++ API.AzureFunctions/API.AzureFunctions.Commons/Responses/Response.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace API.AzureFunctions.Commons.Responses
 {
-    public class Responses
+    public class Response
     {
         public bool IsSuccess { get; set; }
         public string Message { get; set; }
--- API.AzureFunctions/API.AzureFunctions.Functions/Entities/TodoEntity.cs
+++ API.AzureFunctions/API.AzureFunctions.Functions/Entities/TodoEntity.cs
@@ -1,9 +1,5 @@
-﻿using Microsoft.WindowsAzure.Storage.Table;
+﻿using Microsoft.Azure.Cosmos.Table;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace API.AzureFunctions.Functions.Entities
 {
